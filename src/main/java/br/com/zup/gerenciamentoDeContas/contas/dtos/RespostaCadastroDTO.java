97c4bbbc932005cbbdff0d4dd03f2e39a4e8cde8
yan-rds <yyan1207@gmail.com>
2021-11-10 16:02:46 -0300

    Criar RespostaCadastroDTO

--- src/main/java/br/com/zup/gerenciamentoDeContas/contas/dtos/RespostaCadastroDTO.java
+++ src/main/java/br/com/zup/gerenciamentoDeContas/contas/dtos/RespostaCadastroDTO.java
@@ -1,2 +1,46 @@
-package br.com.zup.gerenciamentoDeContas.contas.dtos;public class RespostaCadastroDTO {
+package br.com.zup.gerenciamentoDeContas.contas.dtos;
+
+import br.com.zup.gerenciamentoDeContas.contas.enums.Tipo;
+
+
+public class RespostaCadastroDTO {
+    int id;
+    String nome;
+    double valor;
+    Tipo tipo;
+
+    public RespostaCadastroDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
 }
