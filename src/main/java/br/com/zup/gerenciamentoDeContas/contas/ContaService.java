ec6f2b24214f32ef675b95dc8eaa724f71923501
yan-rds <yyan1207@gmail.com>
2021-11-10 13:50:46 -0300

    Criar ContaService

--- src/main/java/br/com/zup/gerenciamentoDeContas/contas/ContaService.java
+++ src/main/java/br/com/zup/gerenciamentoDeContas/contas/ContaService.java
@@ -1,2 +1,16 @@
-package br.com.zup.gerenciamentoDeContas.contas;public class ContaService {
+package br.com.zup.gerenciamentoDeContas.contas;
+
+import br.com.zup.gerenciamentoDeContas.contas.enums.ContaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContaService {
+
+    @Autowired
+    private ContaRepository repository;
+
+    public void cadastrarConta (Conta conta){
+        repository.save(conta);
+    }
 }
