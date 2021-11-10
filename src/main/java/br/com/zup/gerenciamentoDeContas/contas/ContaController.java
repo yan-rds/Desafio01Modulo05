0df9a35bf267f7c3ad87fa7f9b38808d97e02549
yan-rds <yyan1207@gmail.com>
2021-11-10 17:42:11 -0300

    Criar método exibirCadastros na ContaController

--- src/main/java/br/com/zup/gerenciamentoDeContas/contas/ContaController.java
+++ src/main/java/br/com/zup/gerenciamentoDeContas/contas/ContaController.java
@@ -1,11 +1,15 @@
 package br.com.zup.gerenciamentoDeContas.contas;
 
 import br.com.zup.gerenciamentoDeContas.contas.dtos.CadastroDTO;
+import br.com.zup.gerenciamentoDeContas.contas.dtos.GetCadastroDTO;
 import br.com.zup.gerenciamentoDeContas.contas.dtos.RespostaCadastroDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/contas")
@@ -25,5 +29,14 @@ public class ContaController {
         return respostaCadastroDTO;
     }
 
+    @GetMapping
+    public List exibirCadastros (){
+        List <GetCadastroDTO> todosOsCadastros = new ArrayList<>();
+        for (Conta conta : contaService.exibirTodasContas()){
+            GetCadastroDTO getCadastroDTO = conversor.map(conta, GetCadastroDTO.class);
+            todosOsCadastros.add(getCadastroDTO);
+        }
+        return todosOsCadastros;
+    }
 
 }
