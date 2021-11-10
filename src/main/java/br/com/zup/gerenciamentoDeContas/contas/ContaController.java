3af07708a7e4e90277e6021f4084510fb72dc869
yan-rds <yyan1207@gmail.com>
2021-11-10 13:52:43 -0300

    Criar ContaController

---
+++ src/main/java/br/com/zup/gerenciamentoDeContas/contas/ContaController.java
@@ -0,0 +1,17 @@
+package br.com.zup.gerenciamentoDeContas.contas;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contas")
+public class ContaController {
+
+    @Autowired
+    private ContaService contaService;
+
+
+
+
+}
