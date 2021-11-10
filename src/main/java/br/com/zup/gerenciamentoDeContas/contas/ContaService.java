570793c91b31026a2ea6ac659d09b168b5301ef7
yan-rds <yyan1207@gmail.com>
2021-11-10 15:40:41 -0300

    Inserir validação de vencimento ao cadastro

--- src/main/java/br/com/zup/gerenciamentoDeContas/contas/ContaService.java
+++ src/main/java/br/com/zup/gerenciamentoDeContas/contas/ContaService.java
@@ -1,6 +1,7 @@
 package br.com.zup.gerenciamentoDeContas.contas;
 
 import br.com.zup.gerenciamentoDeContas.contas.enums.ContaRepository;
+import br.com.zup.gerenciamentoDeContas.contas.enums.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,11 @@ public class ContaService {
     private ContaRepository repository;
 
     public void cadastrarConta (Conta conta){
+        if (Validadores.pagamentoVencido(conta.dataDeVencimento)){
+            conta.setStatus(Status.VENCIDA);
+        } else{
+            conta.setStatus(Status.AGUARDANDO);
+        }
         repository.save(conta);
     }
 }
