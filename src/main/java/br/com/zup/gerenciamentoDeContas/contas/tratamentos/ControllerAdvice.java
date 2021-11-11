c8ddd2acfd048302f497e9ba34a408423beecb07
yan-rds <yyan1207@gmail.com>
2021-11-11 10:14:27 -0300

    Criar ControllerAdvice

---
+++ src/main/java/br/com/zup/gerenciamentoDeContas/contas/tratamentos/ControllerAdvice.java
@@ -0,0 +1,8 @@
+package br.com.zup.gerenciamentoDeContas.contas.tratamentos;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+}
