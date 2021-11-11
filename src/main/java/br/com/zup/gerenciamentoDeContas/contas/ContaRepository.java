775030a62b4d90057f83e62f79f19727a996727d
yan-rds <yyan1207@gmail.com>
2021-11-11 11:48:08 -0300

    Adicionar filtro de Tipo na ContaRepository

--- src/main/java/br/com/zup/gerenciamentoDeContas/contas/ContaRepository.java
+++ src/main/java/br/com/zup/gerenciamentoDeContas/contas/ContaRepository.java
@@ -1,6 +1,7 @@
 package br.com.zup.gerenciamentoDeContas.contas;
 
 import br.com.zup.gerenciamentoDeContas.contas.enums.Status;
+import br.com.zup.gerenciamentoDeContas.contas.enums.Tipo;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,6 +9,6 @@ import java.util.List;
 public interface ContaRepository extends CrudRepository<Conta, Integer> {
 
     List<Conta> findAllByStatus (Status status);
-
+    List<Conta> findAllByTipo (Tipo tipo);
 
 }
