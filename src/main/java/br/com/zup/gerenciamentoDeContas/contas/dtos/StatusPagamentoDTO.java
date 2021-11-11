0007d986caa1fc4bb023ce5e0b743a6f3864258a
yan-rds <yyan1207@gmail.com>
2021-11-11 10:06:24 -0300

    Criar StatusPagamentoDTO

--- src/main/java/br/com/zup/gerenciamentoDeContas/contas/dtos/StatusPagamentoDTO.java
+++ src/main/java/br/com/zup/gerenciamentoDeContas/contas/dtos/StatusPagamentoDTO.java
@@ -1,2 +1,18 @@
-package br.com.zup.gerenciamentoDeContas.contas.dtos;public class StatusPagamentoDTO {
+package br.com.zup.gerenciamentoDeContas.contas.dtos;
+
+import br.com.zup.gerenciamentoDeContas.contas.enums.Status;
+
+public class StatusPagamentoDTO {
+    private Status status;
+
+    public StatusPagamentoDTO() {
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
