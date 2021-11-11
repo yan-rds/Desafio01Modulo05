e9a0bc216dc83709a6ab796e86b0fff03b30e5e3
yan-rds <yyan1207@gmail.com>
2021-11-11 10:06:09 -0300

    Criar RespostaAtualizacaoDTO

--- src/main/java/br/com/zup/gerenciamentoDeContas/contas/dtos/RespostaAtualizacaoDTO.java
+++ src/main/java/br/com/zup/gerenciamentoDeContas/contas/dtos/RespostaAtualizacaoDTO.java
@@ -1,2 +1,77 @@
-package br.com.zup.gerenciamentoDeContas.contas.dtos;public class RespostaAtualizacaoDTO {
+package br.com.zup.gerenciamentoDeContas.contas.dtos;
+
+import br.com.zup.gerenciamentoDeContas.contas.enums.Status;
+import br.com.zup.gerenciamentoDeContas.contas.enums.Tipo;
+
+import java.time.LocalDate;
+
+
+
+public class RespostaAtualizacaoDTO {
+    int id;
+    String nome;
+    double valor;
+    Tipo tipo;
+    LocalDate dataDeVencimento;
+    String dataDePagamento;
+    Status status;
+
+    public RespostaAtualizacaoDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public String getDataDePagamento() {
+        return dataDePagamento;
+    }
+
+    public void setDataDePagamento(String dataDePagamento) {
+        this.dataDePagamento = dataDePagamento;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
