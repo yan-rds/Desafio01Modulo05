3ab39673f2d20facae868e8ac7d6b677e2aa977a
yan-rds <yyan1207@gmail.com>
2021-11-11 11:47:21 -0300

    Adicionar filtro de Status na ContaRepository

--- src/main/java/br/com/zup/gerenciamentoDeContas/contas/ContaRepository.java
+++ src/main/java/br/com/zup/gerenciamentoDeContas/contas/ContaRepository.java
@@ -1,7 +1,13 @@
 package br.com.zup.gerenciamentoDeContas.contas;
 
+import br.com.zup.gerenciamentoDeContas.contas.enums.Status;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ContaRepository extends CrudRepository<Conta, Integer> {
+
+    List<Conta> findAllByStatus (Status status);
+
 
 }
