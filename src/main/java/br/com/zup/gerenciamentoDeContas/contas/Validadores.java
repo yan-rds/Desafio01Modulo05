b707f100c0faa86ceacc07910fac956ea605a32a
yan-rds <yyan1207@gmail.com>
2021-11-11 10:05:36 -0300

    Criar método statusValido na Classe Validadores

--- src/main/java/br/com/zup/gerenciamentoDeContas/contas/Validadores.java
+++ src/main/java/br/com/zup/gerenciamentoDeContas/contas/Validadores.java
@@ -1,5 +1,7 @@
 package br.com.zup.gerenciamentoDeContas.contas;
 
+import br.com.zup.gerenciamentoDeContas.contas.enums.Status;
+
 import java.time.LocalDate;
 
 public class Validadores {
@@ -10,4 +12,17 @@ public class Validadores {
         }
         return false;
     }
+
+    public static void statusValido (Conta conta, Status status){
+        if (!status.equals(Status.PAGO)){
+            throw new RuntimeException();
+        }
+        else if (conta.status.equals(Status.PAGO)){
+            throw new RuntimeException();
+        }
+
+    }
+
+
+
 }
