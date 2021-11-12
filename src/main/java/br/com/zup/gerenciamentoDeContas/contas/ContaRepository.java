6be863f53bdf3b3b85bb7d8c56761c24a30f65cf
yan-rds <yyan1207@gmail.com>
2021-11-12 11:06:04 -0300

    Criar filtro de valorAproximado na repository

--- src/main/java/br/com/zup/gerenciamentoDeContas/contas/ContaRepository.java
+++ src/main/java/br/com/zup/gerenciamentoDeContas/contas/ContaRepository.java
@@ -2,13 +2,20 @@ package br.com.zup.gerenciamentoDeContas.contas;
 
 import br.com.zup.gerenciamentoDeContas.contas.enums.Status;
 import br.com.zup.gerenciamentoDeContas.contas.enums.Tipo;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ContaRepository extends CrudRepository<Conta, Integer> {
+public interface ContaRepository extends CrudRepository<Contas, Integer> {
 
-    List<Conta> findAllByStatus (Status status);
-    List<Conta> findAllByTipo (Tipo tipo);
+    List<Contas> findAllByStatus (Status status);
+    List<Contas> findAllByTipo (Tipo tipo);
+    @Query(value = "SELECT * FROM contas WHERE VALOR BETWEEN :valor*0.7 AND :valor*1.3", nativeQuery = true)
+    List <Contas> findAllAproximatedContas (double valor);
+
+
+
 
 }
